655be175fe6d71a65fd653347a92282349036bb0
Ramim Siddiqui <ramimalisiddiqui0@gmail.com>
2024-12-14 11:27:26 +0500

    react-to-do-list

---
+++ react-to-do-list-app-main/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Herosection from './components/Herosection';
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Herosection />
+    </div>
+  );
+}
+
+export default App;
